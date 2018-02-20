3d0febaa01b14d70c200c56e42ea95d145c78f0f
Glenn Brand <grbrand@shaw.ca>
2018-02-19 16:29:21 -0800

    Add files via upload

---
+++ ComplyNowDocumentDbModelManager/Helpers/Config.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+
+namespace ComplyNowDocumentDbModelManager.Helpers
+{
+    public static class DocumentDbConfig
+    {
+        public static string DocDbEndpoint
+        {
+            get
+            {
+                var endPoint = GetEnvironmentVariable("docdb.endpoint");
+                return endPoint;
+            }
+        }
+
+        public static string DocDbAuth
+        {
+            get
+            {
+                var authorizationKey = GetEnvironmentVariable("docdb.auth");
+                return authorizationKey;
+            }
+        }
+
+        public static string DocDbDatabase
+        {
+            get
+            {
+                var doumentDbDatabase = GetEnvironmentVariable("docdb.database");
+                return doumentDbDatabase;
+            }
+        }
+
+        public static string DeviceCollection
+        {
+            get
+            {
+                var deviceCollection = GetEnvironmentVariable("DeviceCollection");
+                return deviceCollection;
+            }
+        }
+
+        public static string GetEnvironmentVariable(string name)
+        {
+            return System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
---
+++ ComplyNowDocumentDbModelManager/Helpers/DocumentClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Azure.Documents.Client;
+
+namespace ComplyNowDocumentDbModelManager.Helpers
+{
+    internal static class DocumentClientFactory
+    {
+        public static DocumentClient Create()
+        {
+            return new DocumentClient(new Uri(DocumentDbConfig.DocDbEndpoint), DocumentDbConfig.DocDbAuth);
+        }
+    }
+}
