f32f43ea67f54b55a1b9a35bcddbb4dee596ad61
Glenn Brand <grbrand@shaw.ca>
2018-02-19 16:28:36 -0800

    Add files via upload

---
+++ ComplyNowDocumentDbModelManager/Models/ComplianceManagerRequestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace ComplyNowDocumentDbModelManager.Models
+{
+    public class ComplianceManagerRequestResponse
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+
+        public bool IsAlreadySavedStatus { get; set; }
+
+        public string ResponseMessage { get; set; }
+
+        public int RecordsSaved { get; set; }
+    }
+}
