74b883b88547f2061497d3bb954296897342ee3d
Glenn Brand <grbrand@shaw.ca>
2018-02-19 16:24:06 -0800

    Add files via upload

---
+++ DocumentDbRepository-master/DocumentDbRepository/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Azure DocumentDb Repository Pattern")]
+[assembly: AssemblyDescription("Azure DocumentDb Repository Pattern is a layer on top of Azure DocumentDb Client that simplifies the communication with the database and provides a repository-like interface.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Santhos")]
+[assembly: AssemblyProduct("DocumentDbRepository")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8f95fe79-b39b-425b-9a7b-0b52bbc5d6c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.3.0.*")]
